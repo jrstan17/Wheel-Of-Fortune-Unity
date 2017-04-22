5ae55821adc5e0cc2b0988a4330d6bf5d6fa512c
Jason Stanley <jrstan17@gmail.com>
2017-04-21 20:54:40 -0400

    Make randomcolorchanger more efficient

--- Assets/Scripts/RandomColorChanger.cs
+++ Assets/Scripts/RandomColorChanger.cs
@@ -6,22 +6,32 @@ using UnityEngine.UI;
 public class RandomColorChanger : MonoBehaviour {
 
     public Text[] TextObjects;
+    public SpriteRenderer[] SpriteObjects;
     public float Speed;
 
     internal Color32 start;
     internal Color32 finish;
     internal static bool isStarted = false;
     internal float elapsedTime = 0;
+    internal List<SpriteRenderer> Subset;
 
 	// Use this for initialization
 	void Start () {
         start = GetRandomColor();
         finish = GetRandomColor();
+
+        Subset = new List<SpriteRenderer>();
     }
 	
 	public void StartColorChange() {
         isStarted = true;
         StartCoroutine(Next());
+    }
+
+    public void SetSubset(List<Trilon> trilons) {
+        foreach(SpriteRenderer sr in SpriteObjects) {
+            Subset.Add(sr);
+        }
     }
 
     public void StopColorChange() {
@@ -35,11 +45,19 @@ public class RandomColorChanger : MonoBehaviour {
             float b = Mathf.Lerp(start.b, finish.b, elapsedTime);
             Color32 nextColor = new Color32((byte)r, (byte)g, (byte)b, 255);
 
-            foreach(Text t in TextObjects) {
-                t.color = nextColor;
+            if (TextObjects.Length != 0) {
+                foreach (Text t in TextObjects) {
+                    t.color = nextColor;
+                }
             }
 
-            elapsedTime += 0.01f;
+            if (Subset != null && Subset.Count != 0) {
+                foreach (SpriteRenderer sr in Subset) {
+                    sr.color = nextColor;
+                }
+            }
+
+            elapsedTime += (Speed / 1000);
 
             if (elapsedTime > 1) {
                 elapsedTime = 0;
@@ -47,7 +65,7 @@ public class RandomColorChanger : MonoBehaviour {
                 finish = GetRandomColor();
             }
 
-            yield return new WaitForSeconds(Speed);
+            yield return new WaitForEndOfFrame();
         }        
     }
 
