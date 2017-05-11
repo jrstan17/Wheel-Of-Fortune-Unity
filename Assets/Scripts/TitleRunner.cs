b31af318b066fba90ce6ada5f89d57c72db7a303
Jason Stanley <jrstan17@gmail.com>
2017-05-10 23:36:06 -0400

    Title Screen Changes #1

---
+++ Assets/Scripts/TitleRunner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleRunner : MonoBehaviour {
+
+    public AudioTracks MusicTracks;
+
+	// Use this for initialization
+	void Start () {
+        MusicTracks.Play("theme");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
