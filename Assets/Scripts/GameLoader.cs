b1eced3c3cf3f442e171c33e1e896b6d8088c491
Jason Stanley <jrstan17@gmail.com>
2017-06-21 20:01:28 -0400

    Begin implementing loading screen

---
+++ Assets/Scripts/GameLoader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameLoader : MonoBehaviour {
+
+    private bool loadScene = false;
+
+    [SerializeField]
+    private int scene;
+
+    // Updates once per frame
+    void Update() {
+        if (!loadScene) {
+            loadScene = true;
+            StartCoroutine(LoadNewScene());
+        }
+    }
+
+
+    // The coroutine runs on its own at the same time as Update() and takes an integer indicating which scene to load.
+    IEnumerator LoadNewScene() {
+
+        yield return new WaitForSeconds(5f);
+
+        // Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
+        AsyncOperation async = SceneManager.LoadSceneAsync(scene);
+
+        // While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
+        while (!async.isDone) {
+            yield return null;
+        }
+    }
+}
