4219da849d14e0a36a8df9160255e514c5c80f43
Jason Stanley <jrstan17@gmail.com>
2017-07-08 11:51:55 -0400

    Fix animation restarting when exiting options menu

--- Assets/Scripts/RevealCanvas.cs
+++ Assets/Scripts/RevealCanvas.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 
 public class RevealCanvas : MonoBehaviour {
 
-    public GameObject NewGamePanel;
-    public GameObject TitleButtonPanel;
-    public SpriteRenderer TitleRenderer;
-    public SpriteRenderer CreditRenderer;
-    public Texture2D CursorTexture;
+	public GameObject NewGamePanel;
+	public GameObject TitleButtonPanel;
+	public SpriteRenderer TitleRenderer;
+	public SpriteRenderer CreditRenderer;
+	public Texture2D CursorTexture;
 
-    public GameObject OptionPanel;
-    public GameObject MainPanels;
+	public GameObject OptionPanel;
+	public GameObject MainPanels;
 
-    public void NewGame_Clicked() {
-        NewGamePanel.SetActive(true);
-        TitleRenderer.enabled = false;
-        CreditRenderer.enabled = false;
-        TitleButtonPanel.SetActive(false);
-    }
+	public void NewGame_Clicked() {
+		NewGamePanel.SetActive(true);
+		TitleRenderer.enabled = false;
+		CreditRenderer.enabled = false;
+		TitleButtonPanel.SetActive(false);
+	}
 
-    public void Exit_Clicked() {
-        Application.Quit();
-    }
+	public void Exit_Clicked() {
+		Application.Quit();
+	}
 
-    public void Options_Clicked() {
-        MainPanels.SetActive(false);
-        OptionPanel.SetActive(true);
-    }
+	public void Options_Clicked() {
+		MainPanels.GetComponent<VisabilityToggler>().ToggleVisability(false);
+		OptionPanel.SetActive(true);
+	}
 
-    public void CloseOptions() {
-        MainPanels.SetActive(true);
-        OptionPanel.SetActive(false);
-    }
+	public void CloseOptions() {
+		MainPanels.GetComponent<VisabilityToggler>().ToggleVisability(true);
+		OptionPanel.SetActive(false);
+	}
 }
