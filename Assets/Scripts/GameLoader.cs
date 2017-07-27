de2196456c2348e34000c12e701a1e56511d1a75
Jason Stanley <jrstan17@gmail.com>
2017-07-27 12:02:03 -0400

    Filter out bonus puzzles that include punctuation

--- Assets/Scripts/GameLoader.cs
+++ Assets/Scripts/GameLoader.cs
@@ -7,9 +7,6 @@ public class GameLoader : MonoBehaviour {
 
     private bool loadScene = false;
 
-    [SerializeField]
-    private int scene;
-
     // Updates once per frame
     void Update() {
         if (!loadScene) {
@@ -23,7 +20,7 @@ public class GameLoader : MonoBehaviour {
     // The coroutine runs on its own at the same time as Update() and takes an integer indicating which scene to load.
     IEnumerator LoadNewScene() {
         // Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
-        AsyncOperation async = SceneManager.LoadSceneAsync(scene);
+        AsyncOperation async = SceneManager.LoadSceneAsync(0);
 
         // While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
         while (!async.isDone) {
--- Assets/Scripts/Puzzle/PuzzleFactory.cs
+++ Assets/Scripts/Puzzle/PuzzleFactory.cs
@@ -5,109 +5,121 @@ using UnityEngine;
 
 public class PuzzleFactory {
 
-    public static int REGULAR_ROUND_PUZZLE_SIZE_MIN = 16;
-    public static int BONUS_ROUND_PUZZLE_SIZE_MAX = 16;
-	public static float BONUS_MAX_RSTLNE_RATIO = 0.3f;
-    const int TIMEOUT = 1000000;
+  public static int REGULAR_ROUND_PUZZLE_SIZE_MIN = 16;
+  public static int BONUS_ROUND_PUZZLE_SIZE_MAX = 16;
+  public static float BONUS_MAX_RSTLNE_RATIO = 0.3f;
+  const int TIMEOUT = 1000000;
 
-    List<Puzzle> Puzzles;
-    int previousPuzzleIndex = -1;
+  List<Puzzle> Puzzles;
+  int previousPuzzleIndex = -1;
 
-    public PuzzleFactory(TextAsset textAsset) {
-        InitPuzzleList(textAsset);
+  public PuzzleFactory(TextAsset textAsset) {
+    InitPuzzleList(textAsset);
+  }
+
+  public Puzzle NewPuzzle(RoundType roundType) {
+    int randomIndex = -1;
+    randomIndex = Random.Range(0, Puzzles.Count);
+
+    int i = 1;
+    while (previousPuzzleIndex == randomIndex || !IsRandomRoundPuzzleValid(randomIndex, roundType) || i > TIMEOUT) {
+      randomIndex = Random.Range(0, Puzzles.Count);
+      i++;
     }
 
-    public Puzzle NewPuzzle(RoundType roundType) {
-        int randomIndex = -1;
-        randomIndex = Random.Range(0, Puzzles.Count);
-
-        int i = 1;
-        while (previousPuzzleIndex == randomIndex || !IsRandomRoundPuzzleValid(randomIndex, roundType) || i > TIMEOUT) {
-            randomIndex = Random.Range(0, Puzzles.Count);
-            i++;
-        }
-
-        if (i > TIMEOUT) {
-            Debug.LogError("Puzzle Generation Timeout!");
-            return null;
-        }
-
-        previousPuzzleIndex = randomIndex;
-
-        return Puzzles[randomIndex];
+    if (i > TIMEOUT) {
+      Debug.LogError("Puzzle Generation Timeout!");
+      return null;
     }
 
-    public Puzzle NewPuzzle(string text) {
-        text = text.ToUpper();
-        return new Puzzle(text + "\tCustom Puzzle\t10/16/2014", 1);
+    previousPuzzleIndex = randomIndex;
+
+    return Puzzles[randomIndex];
+  }
+
+  public Puzzle NewPuzzle(string text) {
+    text = text.ToUpper();
+    return new Puzzle(text + "\tCustom Puzzle\t10/16/2014", 1);
+  }
+
+  public bool IsRandomRoundPuzzleValid(int randomIndex, RoundType type) {
+    string answer = Puzzles[randomIndex].Text;
+    return IsRandomRoundPuzzleValid(answer, type);
+  }
+
+  public bool IsRandomRoundPuzzleValid(string answer, RoundType type) {
+    int letters = 0;
+    foreach (char c in answer) {
+      if (char.IsLetter(c)) {
+        letters++;
+      }
     }
 
-    public bool IsRandomRoundPuzzleValid(int randomIndex, RoundType type) {
-        string answer = Puzzles[randomIndex].Text;
-		return IsRandomRoundPuzzleValid(answer, type);
+    if (type == RoundType.Regular) {
+      if (letters >= REGULAR_ROUND_PUZZLE_SIZE_MIN) {
+        return true;
+      }
+    } else if (type == RoundType.Bonus) {
+      if (letters <= BONUS_ROUND_PUZZLE_SIZE_MAX &&
+       IsValidLetterRatio(answer, Utilities.RSTLNE, BONUS_MAX_RSTLNE_RATIO) &&
+       HasNoPunctuation(answer)) {
+        return true;
+      }
     }
 
-	public bool IsRandomRoundPuzzleValid(string answer, RoundType type){
-		int letters = 0;
-		foreach (char c in answer) {
-			if (char.IsLetter(c)) {
-				letters++;
-			}
-		}
+    return false;
+  }
 
-		if (type == RoundType.Regular) {
-			if (letters >= REGULAR_ROUND_PUZZLE_SIZE_MIN) {
-				return true;
-			}
-		} else if (type == RoundType.Bonus) {
-			if (letters <= BONUS_ROUND_PUZZLE_SIZE_MAX && IsValidLetterRatio(answer, Utilities.RSTLNE, BONUS_MAX_RSTLNE_RATIO)) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-    internal bool IsValidLetterRatio(string answer, List<char> lettersToCheck, float maxRatioAllowed) {
-        List<char> answerLetters = new List<char>();
-        foreach(char c in answer) {
-            if (char.IsLetter(c)) {
-                answerLetters.Add(c);
-            }
-        }
-
-        int sum = 0;
-        int total = 0;
-        foreach(char c in answerLetters) {
-            if (lettersToCheck.Contains(c)) {
-                sum++;
-            }
-
-            total++;
-        }
-
-        float ratio = (float) sum / total;
-        Debug.Log("RSTLNE: " + sum + " / Letters: " + total + " / Ratio: " + ratio.ToString("P"));
-
-		return (ratio <= maxRatioAllowed && ratio != 0);
+  internal bool IsValidLetterRatio(string answer, List<char> lettersToCheck, float maxRatioAllowed) {
+    List<char> answerLetters = new List<char>();
+    foreach (char c in answer) {
+      if (char.IsLetter(c)) {
+        answerLetters.Add(c);
+      }
     }
 
-    internal Puzzle NewPuzzle(int puzzleIndex) {
-        previousPuzzleIndex = puzzleIndex;
-        return Puzzles[puzzleIndex];
+    int sum = 0;
+    int total = 0;
+    foreach (char c in answerLetters) {
+      if (lettersToCheck.Contains(c)) {
+        sum++;
+      }
+
+      total++;
     }
 
-    private void InitPuzzleList(TextAsset textAsset) {
-        if (Puzzles == null) {
-            int count = 1;
-            Puzzles = new List<Puzzle>();
-            string text = textAsset.text;
-            string[] textSplits = textAsset.text.Split('\n');
+    float ratio = (float)sum / total;
+    Debug.Log("RSTLNE: " + sum + " / Letters: " + total + " / Ratio: " + ratio.ToString("P"));
 
-            foreach(string str in textSplits) {
-                Puzzles.Add(new Puzzle(str, count));
-                count++;
-            }
-        }
+    return (ratio <= maxRatioAllowed && ratio != 0);
+  }
+
+  internal Puzzle NewPuzzle(int puzzleIndex) {
+    previousPuzzleIndex = puzzleIndex;
+    return Puzzles[puzzleIndex];
+  }
+
+  private void InitPuzzleList(TextAsset textAsset) {
+    if (Puzzles == null) {
+      int count = 1;
+      Puzzles = new List<Puzzle>();
+      string text = textAsset.text;
+      string[] textSplits = textAsset.text.Split('\n');
+
+      foreach (string str in textSplits) {
+        Puzzles.Add(new Puzzle(str, count));
+        count++;
+      }
     }
+  }
+
+  public static bool HasNoPunctuation(string puzzle) {
+    foreach (char c in puzzle) {
+      if (c != ' ' && !char.IsLetter(c)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
